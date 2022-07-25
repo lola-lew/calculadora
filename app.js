b9367a6cf000dbe4ff86937020a8a8b3eb5e5819
unknown <lolarlew@gmail.com>
2022-07-25 20:01:52 -0300

    listener a cada boton

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 window.addEventListener('load', () => {
   const display = document.querySelector('.calculator-display');
-  const keypadButtons = document.getElementsByClassName('keypad-button')
+  const keypadButtons = document.getElementsByClassName('keypad-button');
+
+  const keypadButtonsArray = Array.from(keypadButtons);
+
+  keypadButtonsArray.forEach((button) => {
+    button.addEventListener('click', () => {
+      console.log(button.innerHTML);
+    })
+  })
 })
